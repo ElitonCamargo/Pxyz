4b61b2850cf751e82c293ab71457a328cf099f1a
Eliton Camargo <camargoliveira@gmail.com>
2024-02-26 14:24:24 -0300

    2º commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App Pxyz</Text>
+      <Text>App Pxyz ......</Text>
       <StatusBar style="auto" />
     </View>
   );
